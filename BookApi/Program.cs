0d55fabaf0e3d2674ccd4969491e19315993e725
Murali <muralibhattu038@gmail.com>
2024-10-26 22:23:49 +0530

    Added the corcs

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Mycorcs", builder =>{
+        builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 var app = builder.Build();
+app.UseCors("Mycorcs");
 
 app.MapControllers();
 //app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => { return Results.Redirect("/api/books"); });
 
 app.Run();
